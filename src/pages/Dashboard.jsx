6252702c5084795f677ed56adcbe1e7e5bd6f721
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-10-23 22:46:59 +0200

    Routing Basics & Loaders

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+import { fetchData } from "../helpers"
+
+export const dashboardLoader = () => {
+  const userName = fetchData('userName');
+  return { userName }
+}
+
+export const Dashboard = () => {
+  const { userName } = useLoaderData();
+
+  return (
+    <div>
+      <h1>{userName}</h1>
+      Dashboard</div>
+  )
+}
